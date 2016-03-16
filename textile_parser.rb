cf8fb35505dc152c48ee6514123333175332db4c
andyvesel <andyvesel@ya.ru>
2016-03-16 12:05:27 +0300

    Add anemone gem (what?)

--- textile_parser.rb
+++ textile_parser.rb
@@ -1,7 +1,15 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'anemone'
 
-doc = Nokogiri::HTML(open("http://serenadatex.ru")) do |config|
-	
+base_url = "http://serenadatex.ru/"
+page_url = "#{base_url}/byaz_ot_proizvoditelya/"
+
+doc = Nokogiri::HTML(open(page_url))
+
+Anemone.crawl(page_url) do |anemone|
+  anemone.on_every_page do |page|
+      puts doc.css('title')
+  end
 end
