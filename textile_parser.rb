26cf863ec990a97cdf2b61d9286afc75372d28a1
andyvesel <andyvesel@ya.ru>
2016-03-17 13:00:04 +0300

    Add anemone crawl method

--- textile_parser.rb
+++ textile_parser.rb
@@ -1,15 +1,9 @@
 require 'rubygems'
-require 'nokogiri'
 require 'open-uri'
 require 'anemone'
 
-base_url = "http://serenadatex.ru/"
-page_url = "#{base_url}/byaz_ot_proizvoditelya/"
-
-doc = Nokogiri::HTML(open(page_url))
-
-Anemone.crawl(page_url) do |anemone|
-  anemone.on_every_page do |page|
-      puts doc.css('title')
-  end
+Anemone.crawl("http://homeandbedroom.ru/") do |anemone|
+  titles = []
+  anemone.on_every_page { |page| titles.push page.doc.at('h1').inner_html rescue nil }
+  anemone.after_crawl { puts titles.compact.sort }
 end
