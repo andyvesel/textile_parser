54e9497e37b1276bab9833560c46499f30e95a44
andyvesel <andyvesel@ya.ru>
2016-03-17 14:58:56 +0300

    Add new file with parse results

--- textile_parser.rb
+++ textile_parser.rb
@@ -1,9 +1,18 @@
 require 'rubygems'
 require 'open-uri'
+require 'nokogiri'
 require 'anemone'
 
-Anemone.crawl("http://homeandbedroom.ru/") do |anemone|
-  titles = []
-  anemone.on_every_page { |page| titles.push page.doc.at('h1').inner_html rescue nil }
-  anemone.after_crawl { puts titles.compact.sort }
+Anemone.crawl("http://serenadatex.ru/") do |anemone|
+  @text = []
+  anemone.on_pages_like(/kpb_avtoralli_d/) { |page| @text.push page.doc.css('title').inner_html rescue nil }
+  anemone.after_crawl { puts @text.compact.sort }
 end
+
+out_file = File.new("out.txt", "w")
+out_file.puts(@text)
+out_file.close
+# anemone.on_pages_like(/kpb_avtoralli_d/) do |page| 
+#   puts page.doc.css('p') 
+#   puts "============"
+# end
