d46f27c681c994a11490138c217765195f18d81b
andyvesel <andyvesel@ya.ru>
2016-03-21 11:39:41 +0300

    Some fixes

--- textile_parser.rb
+++ textile_parser.rb
@@ -1,18 +1,14 @@
 require 'rubygems'
 require 'open-uri'
-require 'nokogiri'
-require 'anemone'
+require 'bundler'
+Bundler.require
 
 Anemone.crawl("http://serenadatex.ru/") do |anemone|
-  @text = []
-  anemone.on_pages_like(/kpb_avtoralli_d/) { |page| @text.push page.doc.css('title').inner_html rescue nil }
-  anemone.after_crawl { puts @text.compact.sort }
+  @output = []
+  anemone.on_pages_like(/kpb_avtoralli_d/) { |page| @output.push page.doc.css('title').inner_html rescue nil }
+  anemone.after_crawl { puts @output.compact.sort }
 end
 
 out_file = File.new("out.txt", "w")
-out_file.puts(@text)
+out_file.puts(@output)
 out_file.close
-# anemone.on_pages_like(/kpb_avtoralli_d/) do |page| 
-#   puts page.doc.css('p') 
-#   puts "============"
-# end
