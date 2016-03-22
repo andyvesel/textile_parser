3d6d24cb1b22948219feedbb2e6a30e9c31f8f68
andyvesel <andyvesel@ya.ru>
2016-03-22 11:47:36 +0300

    Add config file

--- textile_parser.rb
+++ textile_parser.rb
@@ -1,9 +1,12 @@
 require 'rubygems'
 require 'open-uri'
+require 'yaml'
 require 'bundler'
 Bundler.require
 
-Anemone.crawl("http://serenadatex.ru/") do |anemone|
+url = YAML::load(File.open('config.yml'))
+
+Anemone.crawl(url) do |anemone|
   @output = []
   anemone.on_pages_like(/kpb_avtoralli_d/) { |page| @output.push page.doc.css('title').inner_html rescue nil }
   anemone.after_crawl { puts @output.compact.sort }
