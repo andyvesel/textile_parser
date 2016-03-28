7ee9db7aa532c9e03f17a97a392d34bcd65de565
andy <andyvesel@ya.ru>
2016-03-28 21:11:54 +0300

    Wrap methods into class

--- textile_parser.rb
+++ textile_parser.rb
@@ -4,14 +4,29 @@ require 'yaml'
 require 'bundler'
 Bundler.require
 
-url = YAML::load(File.open('config.yml'))
+class Parser
+  
+  def initialize
+    @url = YAML::load(File.open('config.yml'))
+  end
+  
+  attr_reader :url
+  
+  def crawl
+    Anemone.crawl(@url) do |anemone|
+      @output = []
+      anemone.on_pages_like(/kpb_avtoralli_d/) { |page| @output.push page.doc.css('title').inner_html rescue nil }
+      anemone.after_crawl { puts @output.compact.sort }
+    end
+  end
 
-Anemone.crawl(url) do |anemone|
-  @output = []
-  anemone.on_pages_like(/kpb_avtoralli_d/) { |page| @output.push page.doc.css('title').inner_html rescue nil }
-  anemone.after_crawl { puts @output.compact.sort }
+  def create_file
+    out_file = File.new("out.txt", "w")
+    out_file.puts(@output)
+    out_file.close
+  end
 end
 
-out_file = File.new("out.txt", "w")
-out_file.puts(@output)
-out_file.close
+parser = Parser.new
+parser.crawl
+parser.create_file
